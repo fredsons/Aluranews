3c9fa895e0db0347554e3b7fa4fcdca4f2410d06
fsousaentre <85347233+fredsons@users.noreply.github.com>
2022-11-11 17:22:21 -0300

    finaliza projeto

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: '#C5DFFF',
+          escuro: '#061e3c',
+          hover: '#1057b0'
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
+
+    },
+  },
+  plugins: [],
+}
